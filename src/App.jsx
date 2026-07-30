90072d6a38f89221b20979dcbae037b9e8282757
oscarperezGR <oscarperezlds5@gmail.com>
2026-07-30 19:49:53 +0200

    fix: app.jsx reorganization after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Agents from "./components/agents/Agents";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Banner from './components/header/Banner'; 
+import Banner from "./components/header/Banner";
 
 function App() {
   return (
     <>
-      <Header /> 
+      <Header />
       <Banner />
-
 
       <Agents
         subtitle="NUESTRO EQUIPO"
@@ -17,37 +16,17 @@ function App() {
       />
 
       <div className="agents-wrapper">
-        <Agents
-          name='William Hernández'
-          country='Alicante'
-          image='Willian'        
-        />
-        
-        <Agents
-          name='Willfredy Salcedo'
-          country='Sevilla'
-          image='Willfredy'        
-        />
-        
-        <Agents
-          name='Beatriz Iñiguez'
-          country='Granada'
-          image='Beatriz'        
-        />
-        
-        <Agents
-          name='Oscar Pérez'
-          country='Granada'
-          image='Oscar'        
-        />
-        
-        <Agents
-          name='Margarita Bellido'
-          country='Mallorca'
-          image='Margarita'        
-        />
+        <Agents name="William Hernández" country="Alicante" image="Willian" />
+
+        <Agents name="Willfredy Salcedo" country="Sevilla" image="Willfredy" />
+
+        <Agents name="Beatriz Iñiguez" country="Granada" image="Beatriz" />
+
+        <Agents name="Oscar Pérez" country="Granada" image="Oscar" />
+
+        <Agents name="Margarita Bellido" country="Mallorca" image="Margarita" />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
