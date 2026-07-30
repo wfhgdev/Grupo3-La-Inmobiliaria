a3e1c8ca2e3b62aaa318b890f7316e5947021999
wfhgdev <willifhernandez@gmail.com>
2026-07-30 16:40:11 +0200

    feature: complete flats component with CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,39 @@
-// ingresamos nuestro codigo principal de react, en este caso App.jsx, que es el que se renderiza en el index.html.
+import Flats from "./components/flats/Flats";
+
+function App() {
+  return (
+    <>
+      <h3>Disponibles ahora</h3>
+      <h1>Habitaciones en Madrid</h1>
+      <Flats 
+        image="Flat01"
+        description="Habitación luminosa en Malasaña"
+        neighborhood="Malasaña"
+        distance="8 min a la UCM"
+        bedroomCount={3}
+        wifi={true}
+        position="Exterior"
+      />
+      <Flats 
+        image="Flat02"
+        description="Piso moderno cerca de IE University"
+        neighborhood="Salamanca"
+        distance="5 min al IE University"
+        bedroomCount={4}
+        wifi={true}
+        position="Exterior"
+      />
+      <Flats 
+        image="Flat03"
+        description="Habitación acogedora en Lavapiés"
+        neighborhood="Lavapíes"
+        distance="10 min a la URJC"
+        bedroomCount={2}
+        wifi={true}
+        position="Interior"
+      />
+    </>
+  );
+}
+
+export default App;
--- src/components/flats/Flats.jsx
+++ src/components/flats/Flats.jsx
@@ -1,7 +1,22 @@
-function Flats() {
+import './Flats.css';
+
+const images = import.meta.glob('../../assets/img/*.png', 
+    { eager: true, import: 'default' });
+
+function Flats(props) {
   return (
-    <div>
-      <h1>Flats</h1>
+    <div className="Flats">
+      <img 
+        className='flatImage'
+        src={images[`../../assets/img/${props.image}.png`]}
+        alt={`Picture of ${props.neighborhood} flat`} 
+      />
+      <p className='flatDescription'>{props.description}</p>
+      <p className='flatNeighborhood'>{props.neighborhood}</p>
+      <p className='flatDistance'>{props.distance}</p>
+      <p className='flatBedrooms'>{props.bedroomCount} habitaciones</p>
+      <p className='flatWifi'>{props.wifi ? 'WiFi disponible' : 'Sin WiFi'}</p>
+      <p className='flatPosition'>{props.position}</p>
     </div>
   );
 }
