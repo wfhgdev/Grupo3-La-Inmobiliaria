cc2b52e9180b3e2b6c5a32b00eeb11d33273d91d
wfhgdev <willifhernandez@gmail.com>
2026-07-30 17:33:47 +0200

    feature: css styles added to flats page

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,54 @@ function App() {
   return (
     <>
       <section className="flatsSection">
-        <h3>Disponibles ahora</h3>
-        <h1>Habitaciones en Madrid</h1>
-        <Flats
-          image="Flat01"
-          price={350}
-          available={true}
-          favorite={false}
-          description="Habitación luminosa en Malasaña"
-          neighborhood="Malasaña"
-          distance="8 min a la UCM"
-          bedroomCount={3}
-          wifi={true}
-          position="Exterior"
-        />
-        <Flats
-          image="Flat02"
-          price={390}
-          available={true}
-          favorite={false}
-          description="Piso moderno cerca de IE University"
-          neighborhood="Salamanca"
-          distance="5 min al IE University"
-          bedroomCount={4}
-          wifi={true}
-          position="Exterior"
-        />
-        <Flats
-          image="Flat03"
-          price={320}
-          available={true}
-          favorite={false}
-          description="Habitación acogedora en Lavapiés"
-          neighborhood="Lavapíes"
-          distance="10 min a la URJC"
-          bedroomCount={2}
-          wifi={true}
-          position="Interior"
-        />
+        <div className="flatsHeader">
+          <div>
+            <h3>Disponibles ahora</h3>
+            <h1>Habitaciones en Madrid</h1>
+          </div>
+          <a href="#" className="flatsLink">
+            Ver todas
+          </a>
+        </div>
+
+        <div className="flatsGrid">
+          <Flats
+            image="Flat01"
+            price={350}
+            available={true}
+            favorite={false}
+            description="Habitación luminosa en Malasaña"
+            neighborhood="Malasaña"
+            distance="8 min a la UCM"
+            bedroomCount={3}
+            wifi={true}
+            position="Exterior"
+          />
+          <Flats
+            image="Flat02"
+            price={390}
+            available={false}
+            favorite={false}
+            description="Piso moderno cerca de IE University"
+            neighborhood="Salamanca"
+            distance="5 min al IE University"
+            bedroomCount={4}
+            wifi={true}
+            position="Exterior"
+          />
+          <Flats
+            image="Flat03"
+            price={320}
+            available={true}
+            favorite={false}
+            description="Habitación acogedora en Lavapiés"
+            neighborhood="Lavapiés"
+            distance="10 min a la URJC"
+            bedroomCount={2}
+            wifi={true}
+            position="Interior"
+          />
+        </div>
       </section>
     </>
   );
