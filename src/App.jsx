38d2188b4591e51134b06b0bbfb4cde373d810ac
wfhgdev <willifhernandez@gmail.com>
2026-07-30 20:32:04 +0200

    feature: Add About component and update App.jsx to include it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Agents from "./components/agents/Agents";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Banner from "./components/header/Banner";
 import Flats from "./components/flats/Flats";
+import About from "./components/about/About";
+
 
 function App() {
   return (
     <>
+      <Header />
+      <Banner />
       <section className="flatsSection">
         <div className="flatsHeader">
           <div>
@@ -53,6 +61,28 @@ function App() {
           />
         </div>
       </section>
+      <About />
+      <Agents
+        subtitle="NUESTRO EQUIPO"
+        title="Conoce a nuestros agentes"
+        description="Expertos en los barrios universitarios de Madrid, listos para encontrar la habitación perfecta para ti."
+      />
+
+      <div className="agents-wrapper">
+        <Agents name="William Hernández" country="Alicante" image="Willian" />
+
+        <Agents name="Willfredy Salcedo" country="Sevilla" image="Willfredy" />
+
+        <Agents name="Beatriz Iñiguez" country="Granada" image="Beatriz" />
+
+        <Agents name="Oscar Pérez" country="Granada" image="Oscar" />
+
+        <Agents name="Margarita Bellido" country="Mallorca" image="Margarita" />
+      </div>
+      <Footer />
+
+      
+      
     </>
   );
 }
