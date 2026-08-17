fa5a42fd59f99abe27815572324695a32a822fa4
wfhgdev <willifhernandez@gmail.com>
2026-08-17 21:57:37 +0200

    fix: components imports fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
+import FooterComp from "./components/footer/FooterComp";
+import HeaderComp from "./components/header/HeaderComp";
 import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 // import Flats from "./pages/flats/Flats"
-// import Home from "./pages/home"
-// import Agents from "./pages/agents"
+//import Home from "./pages/home/Home"
+// import Agents from "./pages/agents/Agents"
 
 function App() {
   return (
     <>
-      <Header />
+      <HeaderComp />
       <main className="p-6">
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
@@ -19,7 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
-      <Footer />
+      <FooterComp />
     </>
   );
 }
