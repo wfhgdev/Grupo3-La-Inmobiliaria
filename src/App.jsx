2f96e5622f0e150d418955228e133e34fb6263b2
wfhgdev <willifhernandez@gmail.com>
2026-08-17 21:22:23 +0200

    fix: flats componet rename to avoid name collitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-// import Agents from "./components/agents/Agents";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-// import Banner from "./components/header/Banner";
-// import Flats from "./components/flats/Flats";
-// import About from "./components/about/About";
 import "./styles/App.css";
-//nuevos:
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
-// import Flats from "./pages/flats"
+import Flats from "./pages/flats/Flats"
 // import Home from "./pages/home"
 // import Agents from "./pages/agents"
 
@@ -18,9 +13,9 @@ function App() {
       <Header />
       <main className="p-6">
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/flats" element={<Flats />} />
-          <Route path="/agents" element={<Agents />} /> */}
+          {/* <Route path="/agents" element={<Agents />} /> */}
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
