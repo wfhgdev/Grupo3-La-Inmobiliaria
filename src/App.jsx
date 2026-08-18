5d7f3fe356856cf43a7244df06e073a7672ae0ce
Beatriz484 <bea.i.c81@gmail.com>
2026-08-18 10:29:22 +0200

    create the branch Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 // import Flats from "./pages/flats/Flats"
-//import Home from "./pages/home/Home"
+import Home from "./pages/home/Home"
 // import Agents from "./pages/agents/Agents"
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <HeaderComp />
       <main className="p-6">
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           {/* <Route path="/flats" element={<Flats />} /> */}
           {/* <Route path="/agents" element={<Agents />} /> */}
           <Route path="/about" element={<About />} />
