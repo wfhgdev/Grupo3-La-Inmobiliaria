c945294c10769a58784ca658d72ef7fa5f0ed435
oscarperezGR <oscarperezlds5@gmail.com>
2026-08-20 11:08:16 +0200

    feat: add previwe of meals

--- src/pages/meals/meals.jsx
+++ src/pages/meals/meals.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [meals, setMeals] = useState([]);
 
   useEffect(() => {
     const getMeals = async () => {
@@ -9,7 +10,7 @@ function App() {
         "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canada"
       );
 
-      console.log(response);
+      setMeals(response.data.meals);
     };
 
     getMeals();
@@ -19,6 +20,14 @@ function App() {
   return (
     <main>
       <h1>Comidas</h1>
+      <div>
+        {meals.map((meal) => (
+          <div key={meal.idMeal}>
+            <h2>{meal.strMeal}</h2>
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
