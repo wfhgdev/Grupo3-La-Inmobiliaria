528621fabcd138f0f78b2bd27ee353b43d3cc17d
wfhgdev <willifhernandez@gmail.com>
2026-08-21 11:18:05 +0200

    fix: CSS footer snake case changed with camel case

--- src/components/footer/FooterComp.jsx
+++ src/components/footer/FooterComp.jsx
@@ -8,41 +8,41 @@ import emailIcon from "../../assets/img/email.png";
 function FooterComp() {
   return (
     <footer className="footer">
-      <div className="footer-logo-container">
-        <div className="footer-logo-icon">
+      <div className="footerLogoContainer">
+        <div className="footerLogoIcon">
           <svg width="18" height="18" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
           </svg>
         </div>
-        <span className="footer-logo-text">
-          <span className="footer-logo-uni">Uni</span>
-          <span className="footer-logo-space">Space</span>
+        <span className="footerLogoText">
+          <span className="footerLogoUni">Uni</span>
+          <span className="footerLogoSpace">Space</span>
         </span>
       </div>
 
-      <div className="footer-section-social">
-        <span className="footer-title">Contacto</span>
-        <div className="social-icons">
-          <img src={facebookIcon} alt="Facebook" className="social-icon" />
-          <img src={instagramIcon} alt="Instagram" className="social-icon" />
+      <div className="footerSectionSocial">
+        <span className="footerTitle">Contacto</span>
+        <div className="socialIcons">
+          <img src={facebookIcon} alt="Facebook" className="socialIcon" />
+          <img src={instagramIcon} alt="Instagram" className="socialIcon" />
         </div>
       </div>
 
-      <div className="footer-item">
-        <img src={phoneIcon} alt="Teléfono" className="contact-icon" />
+      <div className="footerItem">
+        <img src={phoneIcon} alt="Teléfono" className="contactIcon" />
         <span>+34 987123456</span>
       </div>
 
-      <div className="footer-item">
-        <img src={directionIcon} alt="Dirección" className="contact-icon" />
-        <div className="address-text">
+      <div className="footerItem">
+        <img src={directionIcon} alt="Dirección" className="contactIcon" />
+        <div className="addressText">
           <p>Calle de Fuencarral, 72</p>
           <p>28004 Madrid, España</p>
         </div>
       </div>
 
-      <div className="footer-item">
-        <img src={emailIcon} alt="Email" className="contact-icon" />
+      <div className="footerItem">
+        <img src={emailIcon} alt="Email" className="contactIcon" />
         <span>hola@unispace.es</span>
       </div>
     </footer>
